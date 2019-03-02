23af2a4fd30681b10dcec94a9dda5f647a938176
Xander_ <alessandro.cevoli2@studio.unibo.it>
2019-03-02 10:41:37 +0100

    Added Custom Light Sensor and Bumpers, Polished overall test behaviour

--- controllers/phototaxis/phototaxis.py
+++ controllers/phototaxis/phototaxis.py
@@ -9,6 +9,11 @@ import operator
 WHEEL_RADIUS = 0.02
 AXLE_LENGTH = 0.052
 RANGE = float(1024 / 2)
+EPUCK_FRONT_RAD = math.pi / 2
+PI = math.pi
+W_WHEEL = 2 * PI
+MAX_VL = 7.536
+MAX_VR = 7.536
 
 ####################################################################################################
 
@@ -41,29 +46,52 @@ def sensorArray(ids, timestep, getter, pos = [0.0], enable = True):
         
     return ss
 
-def useMax(lvs, rads):
-    res = max(lvs)
-    a = rads[lvs.index(res)]
-    return (res, a)
-
-def compose(lvs, rads):
-    v = [0.0, 0.0]
-
-    for i in range(0, len(lvs)):
-        v[0] += lvs[i]*math.cos(rads[i])
-        v[1] += lvs[i]*math.sin(rads[i])
-    
-    m = v[0]*v[0]+v[1]*v[1]
-    theta = math.atan2(v[1], v[0]) if v[1] > 0 else 0.0
-
+def maximum(magnitudes, radians):
+    m = max(magnitudes)
+    theta = radians[magnitudes.index(m)]
     return (m, theta)
 
-def lightPerceptor(lvs, rads, strat = lambda lvs, rads: (float, float)): 
-    return strat(lvs, rads)
+def compose(magnitudes, radians):
+    v = [0.0, 0.0]
 
-differential = lambda v, w, d: (v+d/2*w, v-d/2*w)
+    for i in range(0, len(magnitudes)):
+        v[0] += magnitudes[i]*math.cos(radians[i])
+        v[1] += magnitudes[i]*math.sin(radians[i])
+    
+    m = math.sqrt(v[0]*v[0]+v[1]*v[1])
+    theta = math.atan2(v[1], v[0])
 
-sign = lambda theta: 1 if theta > math.pi / 2 and theta < 3/2 * math.pi else -1
+    return (m, betweenZeroPi(theta))
+
+def lightPerceptor(magnitudes, radians, strat = lambda magnitudes, radians: (float, float)):
+   return strat(magnitudes, radians)
+
+def obstaclePerceptor(magnitudes, radians, strat = lambda magnitudes, radians: (float, float)):
+    res, theta = strat(magnitudes, radians)
+
+    return (0, EPUCK_FRONT_RAD) if res < 0.5 else (res, theta + PI)
+
+def mergePerception(magnitudes, radians, strat = lambda magnitudes, radians: (float, float)):
+    return strat(magnitudes, radians)
+
+def betweenZeroPi(rad):
+    if (rad < 0):
+        return 2 * PI + rad 
+    else:
+        return rad
+# w = 2*v*sin(theta)/d
+def differential(theta, d): 
+    v = abs(EPUCK_FRONT_RAD - theta)  
+    w = 2 * v * math.sin(theta) / d
+    vl = v+d/2*w
+    vr = v-d/2*w
+
+    return (
+        min(vl, MAX_VL - 1) * -rsign(theta) + 1, 
+        min(vr, MAX_VR - 1) * rsign(theta) + 1
+    )
+
+rsign = lambda theta: 1 if theta > PI / 2 and theta < 3/2 * PI else -1
 
 #########################################################################################################
 
@@ -72,7 +100,7 @@ ds_n = 8
 ls_n = 9
 motor_n = 2
 pos_n = 2
-bumper_n = 1
+bumper_n = 8
 
 ls_rad = [1.27, 0.77, 0.0, 5.21, 4.21, 3.14159, 2.37, 1.87, 1.58784]
 ds_rad = [1.27, 0.77, 0.0, 5.21, 4.21, 3.14159, 2.37, 1.87]
@@ -89,17 +117,6 @@ light_ids = ids(light_sensor_template, ls_n)
 led_ids = ids(led_template, led_n)
 motors_ids = {'left':'left wheel motor', 'right':'right wheel motor'}
 bumper_ids = ids(bumper_template, bumper_n)
-
-braitenberg_coefficients = [
-    [0.942, -0.22], 
-    [0.63, -0.1], 
-    [0.5, -0.06],
-    [-0.06, -0.06],
-    [-0.06, -0.06], 
-    [-0.06, 0.5], 
-    [-0.19, 0.63], 
-    [-0.13, 0.942]
-]
 
 # create the Robot instance.
 robot = Robot()
@@ -123,52 +140,55 @@ bumper = sensorArray(bumper_ids, timestep, lambda name: robot.getTouchSensor(nam
 for k, m in motor.items():
     m.device.setPosition(float('+inf'))
     m.device.setVelocity(0.0)
-    
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     # Read the sensors:
     
-    speed = [0.0, 0.0] # 0 = sx, 1 = dx
-
     ds_values = []
     ls_values = []
     bumper_value = bumper[0].device.getValue()
-    
+        
+    print(f"Bumper:{bumper_value}")
+
+    # if bumper_value == 1.0: break
+
     for k, s in ds.items(): ds_values.append(s.device.getValue())
     for k, s in ls.items(): ls_values.append(s.device.getValue())
     
-    print(ds_values)
-    print(ls_values)
+    print(f"distance:{ds_values}")
+    print(f"light:{ls_values}")
     
     # Process sensor data here.
+    magnitudes = []
+    radians = []
 
-    res, theta = lightPerceptor(ls_values, ls_rad, compose)
+    # res, theta = lightPerceptor(ls_values, ls_rad, compose)
+    # magnitudes.append(res)
+    # radians.append(theta)
+    # print(f"light direction:{theta}, magnitude: {res}")
 
-    print(f"light direction:{theta}")
+    res, theta = obstaclePerceptor(ds_values, ds_rad, maximum)
+    magnitudes.append(res)
+    radians.append(theta)
+    print(f"obstacle direction:{theta}, distance:{res}")
+    
+    if (len(magnitudes) > 1):
+        res, theta = mergePerception(magnitudes, radians, maximum)
+        print(f"direction:{theta}, magnitude:{res}")
 
-    # if a >= math.pi / 2 - 0.05 and a <= math.pi / 2 + 0.05:
-    #     for i in range(0, motor_n):
-    #         for j in range(0, ds_n):
-    #             speed[i] += braitenberg_coefficients[j][i] * (1.0 - ds_values[j] / RANGE)
-    # else:
-    arc = AXLE_LENGTH / 2 * abs(math.pi / 2 - theta)
-    lv, rv = differential(arc, 2*math.pi, AXLE_LENGTH)
-    # speed[0] = arc * -sign(a) * 50
-    # speed[1] = arc * sign(a) * 50
-    
-    speed[0] = lv * -sign(theta) * 10
-    speed[1] = rv * sign(theta) * 10
-    
-    print(speed)
-    
-    print(bumper_value)
-    
+    lv, rv = differential(theta, AXLE_LENGTH)
+
+    print(f"Speed:{lv}, {rv}")
+        
     # Enter here functions to send actuator commands:
     
-    motor['left'].device.setVelocity(speed[0])
-    motor['right'].device.setVelocity(speed[1])
+    motor['left'].device.setVelocity(lv)
+    motor['right'].device.setVelocity(rv)
 
     pass
 
 # Enter here exit cleanup code.
+motor['left'].device.setVelocity(0.0)
+motor['right'].device.setVelocity(0.0)
