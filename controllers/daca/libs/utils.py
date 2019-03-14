39786951f11ecb4fcc10976beee9c21c6212ab69
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2019-03-14 17:50:58 +0100

    Changed the simulation log file name creation

--- controllers/daca/libs/utils.py
+++ controllers/daca/libs/utils.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 class NetParameters:
     def __init__(self, 
@@ -75,7 +76,7 @@ class SimulationLog:
 
 
 def saveSimulationLog(log:SimulationLog, directoryPath: str) -> str:
-    file_name = f"{directoryPath}SimLog{log.model_name}.json"
+    file_name = f"{directoryPath}SimLog{log.model_name}-{datetime.datetime.now()}g.json"
     with open(file_name, 'w') as outfile:
         json.dump(log.__dict__, outfile, default= lambda x: x.__dict__)
     return file_name
