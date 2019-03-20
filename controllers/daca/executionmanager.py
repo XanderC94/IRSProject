7166341a722722b6c28883baf54f4502429c8471
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2019-03-20 15:12:14 +0100

    Added the test mode management in the execution manager script

--- controllers/daca/executionmanager.py
+++ controllers/daca/executionmanager.py
@@ -13,32 +13,36 @@ webotArguments = [opt.webotsExecutablePath, "--mode=fast", "--batch"]
 
 #train mode
 def runTrain():
-    changer = ParametersChanger.fromList(opt.changingInfo, bounded=False)
-    while changer.hasNext():
-        parameter = changer.next()
-        for K,V in parameter.items():
-            controllerArgs['parameters'][K] = V
-            print(f"Run with parameters {controllerArgs}")
-            writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
-            subprocess.run(webotArguments)
+        controllerArgs["mode"] = "train"
+        changer = ParametersChanger.fromList(opt.changingInfo, bounded=False)
+        while changer.hasNext():
+                parameter = changer.next()
+                for K,V in parameter.items():
+                        controllerArgs['parameters'][K] = V
+                        print(f"Run with parameters {controllerArgs}")
+                        writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
+                        subprocess.run(webotArguments)
 
 
 #test mode
 def runTest():
-    modelFiles = utils.getAllFilesIn(f"./{opt.modelPath}", "json")
-    for modelPath in modelFiles:
-        controllerArgs["modelPath"] = modelPath
-        print(f"Test with model: {modelPath}")
-        writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
-        subprocess.call([ opt.webotsExecutablePath, webotArguments])
+        controllerArgs["mode"] = "test"   
+        modelFiles = utils.getAllFilesIn(f"./{opt.modelPath}", "json")
+        for modelPath in modelFiles:
+                controllerArgs["modelPath"] = modelPath
+                print(f"Test with model: {modelPath}")
+                writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
+                subprocess.call([ opt.webotsExecutablePath, webotArguments])
             
 
-print("START TRAIN...")
-runTrain()
-print("END TRAIN!")
-"""
-print("START TEST...")
-controllerArgs = loaded_json("../../config/defaultControllerArgs.json")
-runTest()
-print("END TEST...")
-"""
+if opt.executionMode == "train":
+        print("START TRAIN...")
+        runTrain()
+        print("END TRAIN!")
+elif opt.executionMode == "test":
+        print("START TEST...")
+        controllerArgs = loaded_json("../../config/defaultControllerArgs.json")
+        runTest()
+        print("END TEST...")
+else:
+        print("UNKWON MODE")
