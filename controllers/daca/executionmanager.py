b06e1463dac019990054ceb007c4ac49905d0942
XanderC94 <alessandro.cevoli2@studio.unibo.it>
2019-03-21 10:06:58 +0100

    Fix some bugs in executionmanager.py on windows

--- controllers/daca/executionmanager.py
+++ controllers/daca/executionmanager.py
@@ -9,42 +9,44 @@ print(opt)
     
 controllerArgs = loadJsonFile("../../config/defaultControllerArgs.json")
 modifiedControllerArgsFilePath = "../../config/controllerArgs.json"
-webotsBootArguments = [opt.webotsExecutablePath, "--mode=fast", "--batch"] 
+webotsBootArguments = [str(opt.webotsExecutablePath), "--mode=fast", "--batch"] 
 
 #train mode
 def runTrain():
-        webotsBootArguments.append(opt.worldTrainPath) 
-        controllerArgs["mode"] = "train"
-        changer = ParametersChanger.fromList(opt.changingInfo, bounded=False)
-        while changer.hasNext():
-                parameter = changer.next()
-                for K,V in parameter.items():
-                        controllerArgs['parameters'][K] = V
-                        print(f"Run with parameters {controllerArgs}")
-                        writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
-                        subprocess.run(webotsBootArguments)
+    webotsBootArguments.append(opt.worldTrainPath) 
+    controllerArgs["mode"] = "train"
+    changer = ParametersChanger.fromList(opt.changingInfo, bounded=False)
+    while changer.hasNext():
+        parameter = changer.next()
+        for K,V in parameter.items():
+            controllerArgs['parameters'][K] = V
+            
+        print(webotsBootArguments)
+        print(f"Run with parameters {controllerArgs}")
+        writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
+        subprocess.run(webotsBootArguments)
 
 
 #test mode
 def runTest():
-        webotsBootArguments.append(opt.worldTestPath)
-        controllerArgs["mode"] = "test"   
-        modelFiles = getAllFilesIn(f"./{controllerArgs['modelPath']}", "json")
-        print(f"Model path {opt.modelPath}")
-        for modelPath in modelFiles:
-                controllerArgs["modelPath"] = str(modelPath)
-                print(f"Test with model: {modelPath}")
-                writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
-                subprocess.call(webotsBootArguments)
+    webotsBootArguments.append(opt.worldTestPath)
+    controllerArgs["mode"] = "test"   
+    modelFiles = getAllFilesIn(f"./{controllerArgs['modelPath']}", "json")
+    print(f"Model path {opt.modelPath}")
+    for modelPath in modelFiles:
+        controllerArgs["modelPath"] = str(modelPath)
+        print(f"Test with model: {modelPath}")
+        writeJsonOnFile(controllerArgs, modifiedControllerArgsFilePath)
+        subprocess.call(webotsBootArguments)
             
 
 if opt.executionMode == "train":
-        print("START TRAIN...")
-        runTrain()
-        print("END TRAIN!")
+    print("START TRAIN...")
+    runTrain()
+    print("END TRAIN!")
 elif opt.executionMode == "test":
-        print("START TEST...")
-        runTest()
-        print("END TEST...")
+    print("START TEST...")
+    runTest()
+    print("END TEST...")
 else:
-        print("UNKWON MODE")
+    print("UNKWON MODE")
